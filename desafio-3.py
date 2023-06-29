89ce39a4b6e9b4ab6d228d164c09c9371b49e586
Klayton Dias <arqkdias@gmail.com>
2023-06-29 15:33:35 -0400

    create desafio-3.py

---
+++ desafio-3.py
@@ -0,0 +1,8 @@
+#TODO: Criar as condições necessárias para impressão da saída conforme o enunciado.
+
+valorPedido = int(input())
+
+if valorPedido >= 50:
+  print('Parabens, você ganhou uma sobremesa gratis!')
+else:
+  print('Que pena, você nao ganhou nenhum brinde especial.')
